b95a74448a3f158d7a420c73b95a912279b570fb
migueldsw <mdsw@cin.ufpe.br>
2017-07-05 22:57:53 -0300

    gitignore update | general utils added

---
+++ utils/utils.py
@@ -0,0 +1,22 @@
+"""
+General project utils
+"""
+
+DATASET_ROOT_PATH = ''
+IMAGE_LIST_FILE = ''
+COMPLETE_TAGS_FILE = ''
+IMAGES_PATH = ''
+TEXT_DATA_FILE = ''
+TAG_LIST_FILE = ''
+
+
+with open('../config.info') as f:
+    content = f.readlines()
+lines = [x.strip() for x in content]
+
+DATASET_ROOT_PATH = lines[0]
+IMAGE_LIST_FILE = DATASET_ROOT_PATH + lines[1]
+COMPLETE_TAGS_FILE = DATASET_ROOT_PATH + lines[2]
+IMAGES_PATH = DATASET_ROOT_PATH + lines[3]
+TEXT_DATA_FILE = DATASET_ROOT_PATH + lines[4]
+TAG_LIST_FILE = DATASET_ROOT_PATH + lines[5]
