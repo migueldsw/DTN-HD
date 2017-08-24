e220481bec8f86c552e635e87a6a2a860bc7d6e7
migueldsw <mdsw@cin.ufpe.br>
2017-08-23 23:45:35 -0300

    pca code cleaning

--- feature_extractor/pca.py
+++ feature_extractor/pca.py
@@ -1,37 +1,13 @@
 """
 Run PCA
-
-TEST TEST TEST ....
 """
-import numpy as np
 from sklearn.decomposition import PCA
+from dataset.utils import save_cache
 
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-
-pca = PCA(n_components=2)
-
-pca.fit(X)
-
-# print(pca.explained_variance_ratio_)
-
-
-pca = PCA(n_components=2, svd_solver='full')
-x1 = pca.fit_transform(X)
-
-# print(pca.explained_variance_ratio_)
-
-pca = PCA(n_components=1, svd_solver='arpack')
-x2 = pca.fit_transform(X)
-
-
-# print(pca.explained_variance_ratio_)
-
-# print('PCA...')
-# print(X)
-# print(x1)
-# print(x2)
 
 def get_pc(X, n):
-    pca = PCA(n_components=n, svd_solver='arpack')
-    x2 = pca.fit_transform(X)
-    return x2
+    pca = PCA(n_components=n, svd_solver='arpack') #svd_solver='full'
+    px = pca.fit_transform(X)
+    # print(pca.explained_variance_ratio_)
+    save_cache("pcx", px)
+    return px
